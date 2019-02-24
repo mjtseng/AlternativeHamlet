948237934a944edc381d1528de52a853242cea49
Maggie Tseng <mjtseng42@gmail.com>
2019-02-24 15:28:59 -0500

    added js file

---
+++ randomizer.js
@@ -0,0 +1,9 @@
+// javascript for randomizing an event - a 50-50 chance
+
+outcome_num = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
+if (outcome_num % 2 == 0){
+	// something
+}
+else{
+	// other thing
+}
