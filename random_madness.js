925fe02d14675eb6bd90573296a76b9968f9254a
Maggie Tseng <mjtseng42@gmail.com>
2019-02-26 12:52:19 -0500

    linked in insanity and suicide pages

--- random_madness.js
+++ random_madness.js
@@ -8,3 +8,11 @@ outcome_num = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
 if (outcome_num % 20 == 0){
 	document.getElementById("insanity").style.display = "block";
 }
+
+/* to include in html files, use this:
+
+	<script type='text/javascript' language='javascript' src='random_madness.js'/>
+	
+	<a id="insane" style="display:none" href="HamletInsanity.html">You start to feel a little weird...</a>
+
+*/
